64e62c3de4b47f34b38f61649a862023fcd33f70
MorOlesya <dl9morozova@gmail.com>
2026-01-17 22:21:15 +0300

    Implement custom setters for salary and experience

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,2 +1,27 @@
-class Employee {
+class Employee (
+    private val fullName: String,
+    private val position: String,
+    private val salary: Int,
+    private val yearsOfExperience: Int
+) {
+    val Ename: String
+        get() = fullName
+    val Eposition: String
+        get() = position
+    var Esalary: Int = salary
+        set(value) {
+            if (value < 0) {
+                println("Зарплата не может быть отрицательной!")
+            } else {
+                field = value
+            }
+        }
+    var Eexprience: Int = yearsOfExperience
+        set(value) {
+            if (value > 50){
+                println("Макс опыт 50 лет!")
+            } else {
+                field = value
+            }
+        }
 }
